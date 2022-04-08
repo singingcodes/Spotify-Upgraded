0a46ffed76cc4bda0523c12a3d26b5cd52b2d96e
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-04-08 16:42:38 +0200

    push

--- albumPage.js
+++ albumPage.js
@@ -1,3 +1,109 @@
+window.onload = () => {
+            let searchParams = new URLSearchParams(window.location.search) 
+            let id = searchParams.get("songId")
+
+            console.log(id) 
+
+            fetch("https://striveschool-api.herokuapp.com/api/deezer/album/" + id)
+                .then(response => response.json())
+                .then(songs => {
+
+                    console.log(songs)
+                    
+                  let songArr={...songs}
+          let row = document.querySelector(".rowTop")
+          row.innerHTML = ""
+          
+          let col2 = document.createElement("div")
+          col2.classList.add("col-12")
+          col2.classList.add("d-flex")
+          col2.innerHTML=`
+          <div class="col-2">
+          <img
+          id="hero-img"
+          src=${songs.cover}
+          class="img-fluid"
+          alt=${songs.title}
+          />
+          </div>
+          <div class="col-10">
+                    <div class="d-flex flex-column justify-content-end h-100">
+                      <small>ALBUM</small>
+                      <h1>${songs.title}</h1>
+                      <div>
+                        <p>
+                          <small
+                            ><strong>
+                              <img
+                                src=${songs.cover}
+                                height="22px"
+                                alt=""
+                                style="border-radius: 2rem"
+                                alt="Original Album Cover"
+                              />
+                              ${songs.artist.name} . </strong
+                            >${songs.release_date}<strong> . </strong>${songs.nb_tracks}<strong>
+                              ,
+                            </strong>
+                            ${songs.duration}
+                          </small>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>`
+         
+          row.appendChild(col2)
+        // The end of first part
+
+        let row2 = document.querySelector(".trackRow")
+         row2.innerHTML = ""
+
+        let songTrack = songs.tracks.data
+        console.log(songTrack)
+
+        
+      function count() {
+          for(let i = 1; i <= songTrack.length; i++) {
+        console.log(i);
+          }
+      }
+
+      let x = count()
+
+      
+        
+
+        songTrack.forEach(song => {
+
+          let col = document.createElement("div")
+            col.classList.add("col-12")
+            col.classList.add("d-flex")
+            col.innerHTML = `<div id="album-track-list" class="col-5">
+                    <div class="ml-3 pb-4">
+                      <span class="track-num" >${x}</span>
+                      <span class="album-song ml-4"
+                        ><strong class="track-decoration">${song.title}</strong
+                        ><br /><span class="singer-name">${song.artist.name}</span></span
+                      >
+                    </div>
+                  </div>
+                  <div id="album-music-player" class="col-6">
+                    <div class="d-flex flex-column"></div>
+                  </div>
+                  <div id="track-times" class="col-1">
+                    <div class="ml-3 pb-5">${song.duration}</div>
+                  </div>
+                                `
+            row2.appendChild(col)
+        
+          
+        });
+  
+        })
+}
+
+
 
 
 // play pause button
--- homepagefinal.js
+++ homepagefinal.js
@@ -3,7 +3,69 @@ window.onload = function() {
     addListenerToLoveHeart()
     addListenerToFirstButton()
     addEventToArtistName()
+    loadPage()
     }
+
+
+let loadPage = () => {
+
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
+    .then(response => response.json())
+    .then(songs => {
+        let row = document.querySelector(".rowrow")
+        row.innerHTML = ""
+        songs.data.forEach(song => {
+            let col = document.createElement("div")
+            col.classList.add("col-3")
+            col.innerHTML = `<div class="card mb-3">
+                                    <div class="row no-gutters row-remove-margin row-remove-margin">
+                                        <div class="col-4">
+                                            <img class="first-section-img" src=${song.album.cover} alt="...">
+                                        </div>
+                                        <div class="col-6 flex-container">
+                                            <div class="card-body card-body-section1">
+                                                <p class="card-text">${song.album.title}</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-2 flex-container">
+                                            <img class="green-play-img" src="http://hypebot.typepad.com/.a/6a00d83451b36c69e201bb09b0b161970d-200wi" alt="">
+                                        </div>
+                                    </div>
+                                </div>`
+            row.appendChild(col)
+        })
+    })
+
+    fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=queen")
+    .then(response => response.json())
+    .then(songs => {
+        let row = document.querySelector(".queenrow")
+        row.innerHTML = ""
+        songs.data.forEach(song => {
+            let col = document.createElement("div")
+            col.classList.add("col-3")
+            col.innerHTML = `<div class="card mb-3">
+                                    <div class="row no-gutters row-remove-margin row-remove-margin">
+                                        <div class="col-4">
+                                            <img class="first-section-img" src=${song.album.cover} alt="...">
+                                        </div>
+                                        <div class="col-6 flex-container">
+                                            <div class="card-body card-body-section1">
+                                                <p class="card-text">${song.title}</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-2 flex-container">
+                                        <a href="./albumPage.html?songId=${song.album.id}">listen...</a>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                                `
+            row.appendChild(col)
+        })
+    })
+    
+}
 
 
 
